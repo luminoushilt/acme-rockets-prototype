28a65f9b76790b24e671a16b48e77aebcb41b8ba
Hilton Williams <hwilliams@luminoushilt.com>
2022-12-07 22:29:41 -0800

    Build layout for project.

--- main.js
+++ main.js
@@ -1,23 +1,15 @@
-import "./style.css";
-import javascriptLogo from "./javascript.svg";
-import { setupCounter } from "./counter.js";
+const initApp = () => {
+    const hamburgerBtn = document.querySelector("#hamburger-button");
+    const mobileMenu = document.querySelector("#mobile-menu");
 
-document.querySelector("#app").innerHTML = `
-  <div class="self-center">
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`;
+    const toggleMenu = () => {
+        mobileMenu.classList.toggle("hidden");
+        mobileMenu.classList.toggle("flex");
+        hamburgerBtn.classList.toggle("toggle-btn");
+    };
 
-setupCounter(document.querySelector("#counter"));
+    hamburgerBtn.addEventListener("click", toggleMenu);
+    mobileMenu.addEventListener("click", toggleMenu);
+};
+
+document.addEventListener("DOMContentLoaded", initApp);
